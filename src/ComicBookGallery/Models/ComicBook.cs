8248b4ea921aac5ececfe66372ad7233a0920afd
tega23 <tega23@yahoo.com>
2016-12-26 09:04:29 +0100

    Updated comic book model

--- src/ComicBookGallery/Models/ComicBook.cs
+++ src/ComicBookGallery/Models/ComicBook.cs
@@ -14,5 +14,19 @@ namespace ComicBookGallery.Models
         public Artist[] Artists { get; set; }
         public bool Favourite { get; set; }
 
+        public string DisplayText
+        {
+            get
+            {
+                return SeriesTitle + " #" + IssueNumber;
+            }
+        }
+        public string CoverImageFileName
+        {
+            get
+            {
+                return (SeriesTitle.Replace(" ", "-")).ToLower() + IssueNumber + ".jpg";
+            }
+        }
     }
 }
